2992a3dd656347ed1bf5a6d9363568ce33e6f04b
David Venegas <venegasdavidm@gmail.com>
2024-03-06 15:56:37 -0800

    tesla improvments

--- Rocket Lab.js
+++ Rocket Lab.js
@@ -9,16 +9,55 @@ const selections = {
     "What are your salary expectations": "150,000 USD",
     "Please share with us your salary expectations": "150,000 USD",
     "What are your salary expectations in New Zealand Dollars": "100,000",
-    "What is your availability/notice period?": "2 weeks",
+    "What is your availability/notice period?": "Immediately",
     "What is your current time zone?": "Pacific Standard Time",
     "Are you willing to work onsite?": "Yes",
     "If Citizenship Status Other, please explain:": "N/A",
     "Do you meet all the required qualifications": "Yes",
-    "How many years of relevant work experience do you have": "6",
+    "How many years of relevant work experience do you have": "8",
     "Have you previously interviewed at Rocket Lab": "No",
+    "How did you hear about this opportunity?": "I am in the industry",
+    "Please specify": "I work at Stoke Space Technologies, a launch provider",
     "Are you currently, or have you ever been employed by the U.S. Government": "No",
     "Any offer of employment with Rocket Lab is contingent on a successful criminal background check": "Yes",
+    "Any offer of employment with Rocket Lab is contingent": "Yes",
 };
+
+// email [CUSTOM]
+try {
+    const labels = Array.from(document.getElementsByTagName('label'));
+    const targetLabel = labels.find(label => label.textContent.includes('Email'));
+    if (targetLabel) {
+        const input = targetLabel.parentElement.querySelector('input[aria-required="true"]');
+        if (input) {
+            input.value = 'venegas1david1m1@gmail.com';
+        } else {
+            console.warn('Input element for "Email" not found.');
+        }
+    } else {
+        console.warn('Label element for "Email" not found.');
+    }
+} catch (error) {
+    console.error('Error occurred while setting the input:', error);
+}
+
+// phone number [CUSTOM]
+try {
+    const labels = Array.from(document.getElementsByTagName('label'));
+    const targetLabel = labels.find(label => label.textContent.includes('Phone'));
+    if (targetLabel) {
+        const input = targetLabel.parentElement.querySelector('input[type="text"]');
+        if (input) {
+            input.value = '8573273414';
+        } else {
+            console.warn('Input element for "Phone" not found.');
+        }
+    } else {
+        console.warn('Label element for "Phone" not found.');
+    }
+} catch (error) {
+    console.error('Error occurred while setting the input:', error);
+}
 
 try {
     Object.entries(selections).forEach(([labelText, answerValue]) => {
